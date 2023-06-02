f070befb3367a20bc674ec641389e49a9c5e6690
Jai Rajput <jai@tecorb.co>
2023-06-02 23:52:12 +0530

    admin_tamplate

---
+++ app/controllers/admin/home_controller.rb
@@ -0,0 +1,6 @@
+class Admin::HomeController < ApplicationController
+before_action :authenticate_user!
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+  namespace :admin do
+    get 'home/index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
