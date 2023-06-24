6332cc8776025f81e762e30e94361fd9ad322347
Jai Rajput <jai@tecorb.co>
2023-06-25 00:54:42 +0530

    database change

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,7 @@ threads min_threads_count, max_threads_count
 # terminating a worker in development environments.
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+#workers ENV.fetch("WEB_CONCURRENCY") {2}
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
